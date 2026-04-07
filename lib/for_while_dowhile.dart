38767898d42b5fafcb03a1c617f3a9383e7ef6f9
padma270499-hub <padma270499@gmail.com>
2026-04-07 11:16:06 +0530

    for loop

--- lib/for_while_dowhile.dart
+++ lib/for_while_dowhile.dart
@@ -1,7 +1,23 @@
-void main(){
-  for(int i=0;i<=10;i++){
-    if(i%2==0){
-      print(i);
+void main() {
+  for (int i = 0; i <= 10; i++) {
+    if (i % 2 == 0) {
+      print("even no's $i");
     }
+  }
+  // even no's
+  for (int i = 0; i <= 10; i += 2) {
+    print(i);
+  }
+  for (int i = 0; i < 10; i++) {
+    if (i % 2 != 0) {
+      print("odd no's $i");
+    }
+  }
+  //  odd no's
+  for (int i = 1; i <= 10; i += 2) {
+    print(i);
+  }
+  for (int i = 10; i >= 0; i--) {
+    print("reverse no's $i");
   }
 }

328bd2120f418d5de13257c7af2d29443c210d0d
padma270499-hub <padma270499@gmail.com>
2026-04-07 12:46:09 +0530

    repeat sum prime

--- lib/for_while_dowhile.dart
+++ lib/for_while_dowhile.dart
@@ -8,6 +8,7 @@ void main() {
   for (int i = 0; i <= 10; i += 2) {
     print(i);
   }
+  // ================================================
   for (int i = 0; i < 10; i++) {
     if (i % 2 != 0) {
       print("odd no's $i");
@@ -17,7 +18,39 @@ void main() {
   for (int i = 1; i <= 10; i += 2) {
     print(i);
   }
+  //==================================================
   for (int i = 10; i >= 0; i--) {
-    print("reverse no's $i");
+    print("reverse no's $i"); // reverse 10 to 0
+  }
+  //===================================================
+
+  String name = 'padma';
+  for (int i = 0; i < 5; i++) {
+    print(name);
+  } // repeat name 5 times
+
+  //==========================================================
+
+  int sum = 0;
+  for (int i = 1; i <= 10; i++) {
+    sum += i;
+  }
+  print('The sum of the first 10 numbers is: $sum');
+  //==========================================================
+
+  int num = 9;
+  int count = 0;
+
+  for (int i = 1; i <= num; i++) {
+    if (num % i == 0) {
+      count++;
+    }
+  }
+  if (count == 2) {
+    print("The number is prime");
+  } else {
+    print("The number is not a prime");
   }
 }
+
+//=============================================================
